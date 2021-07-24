5f19676c7c3569b316cfb58718c463f5f6da1468
AbrasiveBoar902 <abrasiveboar@qq.com>
2021-07-24 13:20:37 +0800

    初次提交 2.0.8制作中

---
+++ .minecraft/versions/1.17.1-Fabric 0.11.6/mods/Optifine-on.bat
@@ -0,0 +1,27 @@
+cd ..
+cd .minecraft\versions\1.17.1-Fabric 0.11.6\mods
+ren indium-1.0.0+mc1.17.1.jar indium-1.0.0+mc1.17.1.jar.disabled
+ren iris-mc1.17-1.1.0-starline+build.430.jar iris-mc1.17-1.1.0-starline+build.430.jar.disabled
+ren sodium-fabric-mc1.17.1-0.3.0+IRIS1-build.105.jar sodium-fabric-mc1.17.1-0.3.0+IRIS1-build.105.jar.disabled
+
+ren sodium-extra-mc1.17.1-0.3.2.jar sodium-extra-mc1.17.1-0.3.2.jar.disabled
+ren sodium-extra-mc1.17.1-0.3.3.jar sodium-extra-mc1.17.1-0.3.3.jar.disabled
+
+ren sodium-fabric-mc1.17.1-0.3.0+build.527.jar sodium-fabric-mc1.17.1-0.3.0+build.527.jar.disabled
+ren canvas-mc117-1.17-1.0.1816.jar canvas-mc117-1.17-1.0.1816.jar.disabled
+ren BetterSodiumVideoSettingsButton-2.0.0.jar BetterSodiumVideoSettingsButton-2.0.0.jar.disabled
+
+
+ren c2me-fabric-mc1.17.1-0.2.0+alpha.2.3-all.jar c2me-fabric-mc1.17.1-0.2.0+alpha.2.3-all.jar.disabled
+ren connected-block-textures-0.1.5+1.17.jar connected-block-textures-0.1.5+1.17.jar.disabled
+ren IngameIME-fabric-1.17-pre5-1.6.4.jar IngameIME-fabric-1.17-pre5-1.6.4.jar.disabled
+ren lambdabettergrass-1.2.0+1.17.jar lambdabettergrass-1.2.0+1.17.jar.disabled
+ren MasaGadget-1.17-1.1.6.jar MasaGadget-1.17-1.1.6.jar.disabled
+ren satin-1.6.1.jar satin-1.6.1.jar.disabled
+ren tweakfork-fabric-1.17.0-1.2.2.jar tweakfork-fabric-1.17.0-1.2.2.jar.disabled
+ren tweakeroo-fabric-1.17.1-0.10.0-dev.20210710.155746.jar.disabled tweakeroo-fabric-1.17.1-0.10.0-dev.20210710.155746.jar
+ren WorldEditCUI-1.17-pre2+01-SNAPSHOT.jar WorldEditCUI-1.17-pre2+01-SNAPSHOT.jar.disabled
+ren lithium-fabric-mc1.17.1-0.7.3.jar lithium-fabric-mc1.17.1-0.7.3.jar.disabled
+ren optifabric-1.11.18.jar.disabled optifabric-1.11.18.jar
+ren preview_OptiFine_1.17.1_HD_U_G9_pre30.jar.disabled preview_OptiFine_1.17.1_HD_U_G9_pre30.jar
+ren clientcommands-2.5.3.jar clientcommands-2.5.3.jar.disabled
